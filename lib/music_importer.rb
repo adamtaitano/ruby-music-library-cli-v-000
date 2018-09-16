e3f6875c6af3c011d1dd8b463b087a4d79090681
Adam Taitano <adam.taitano@gmail.com>
2018-09-16 18:45:42 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -15,17 +15,4 @@ class MusicImporter
       Song.create_from_filename(file)
     end
   end
-  
-
-  def initialize(path)
-    @path = path
-  end
-
-  def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-  end
-
-  def import
-    files.each{ |f| Song.create_from_filename(f) }
-  end
 end
