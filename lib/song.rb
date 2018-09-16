0d0fce4eee5da75845bbec7248c9d716a0022184
Adam Taitano <adam.taitano@gmail.com>
2018-09-16 18:39:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,21 @@ class Song
  attr_reader :genre, :artist
  @@all = []
  
- def initialize(name, artist = nil, genre = nil)
-  @name = name
-  self.artist = artist if artist
-  self.genre = genre if genre
- end
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
  
  def self.all
   @@all
