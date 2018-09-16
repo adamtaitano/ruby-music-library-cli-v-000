e510108de5c25a4bc2acd9548484fd1297fc871a
Adam Taitano <adam.taitano@gmail.com>
2018-09-16 18:16:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,24 @@
 class Song
  attr_accesor :name
+ @@all = []
  
  def initialize(name)
   @name = name
+ end
+ 
+ def self.all
+  @@all
+ end
+ 
+ def self.destroy_all
+  @@all.clear
+ end
+ 
+ def save
+  @@all << self
+ end
+ 
+ def self.create
+   Song.new.save
  end
 end
