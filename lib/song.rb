9a7f51bf8e95aef41b8b882df7e1e76a9809a55a
Adam Taitano <adam.taitano@gmail.com>
2018-09-16 18:29:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,13 @@ class Song
  end
  
  def self.find_by_name(name)
-  @@all.detect(name) 
+  @@all.detect{|song| song.name == name}
+ end
+ 
+ def self.find_or_create_by_name(name)
+  if self.find_by_name(name)
+    
+  else
+    self.create(name)
  end
 end
