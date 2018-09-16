06fd834187857d662e2a37e2f3e867552eaa8b2e
Adam Taitano <adam.taitano@gmail.com>
2018-09-16 18:34:10 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,3 +1,18 @@
 class MusicImporter
- 
+  @@files = []
+  
+  def initialize(file_path)
+    
+  end
+  
+  def self.files
+    @@files
+  end
+  
+  def self.import
+    @@all.each do |file|
+      Song.new_from_filename(file)
+    end
+  end
+  
 end
